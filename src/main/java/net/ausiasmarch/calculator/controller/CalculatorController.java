0f32dba2a14883cd885a87c5fb50b3748a3e4a9c
Guesanjo <guesanjoupv@gmail.com>
2022-10-02 19:32:31 +0200

    get funcional

--- src/main/java/net/ausiasmarch/calculator/controller/CalculatorController.java
+++ src/main/java/net/ausiasmarch/calculator/controller/CalculatorController.java
@@ -24,24 +24,24 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
     @CrossOrigin
-    @GetMapping("/{op1}/{op2}")
+    @GetMapping("/add/{op1}/{op2}")
     public ResponseEntity<Calculation> add(@PathVariable(value="op1") int op1, @PathVariable(value="op2") int op2) {
    
         return calculatorService.add(op1,op2);
     }
-    
+    @CrossOrigin
     @GetMapping("/minus/{op1}/{op2}")
     public ResponseEntity<Calculation> minus(@PathVariable int op1, @PathVariable int op2) {
    
         return calculatorService.minus(op1,op2);
     }
-
+    @CrossOrigin
     @GetMapping("/multiply/{op1}/{op2}")
     public ResponseEntity<Calculation> multiply(@PathVariable int op1, @PathVariable int op2) {
    
         return calculatorService.multiply(op1,op2);
     }
-
+    @CrossOrigin
     @GetMapping("/div/{op1}/{op2}")
     public ResponseEntity<Calculation> div(@PathVariable int op1, @PathVariable int op2) {
    
